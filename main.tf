f0e6df1797613613aded308f1f1afa71f83f458b
Muthuinc <smramalingamm@gmail.com>
2024-05-15 22:41:17 +0530

    terraform code and docker compose added

---
+++ main.tf
@@ -0,0 +1,82 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "monitor" {
+  name        = "monitoring"
+  description = "For monitoring the server"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+   ingress {
+    description      = "TLS from VPC"
+    from_port        = 9100
+    to_port          = 9100
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 9090
+    to_port          = 9090
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "monitor"
+  }
+}
+
+resource "aws_instance" "instance1" {
+  ami                     = var.instance_ami
+  instance_type           = var.instance_type
+  subnet_id               = "subnet-00c957f11476482a1"
+  vpc_security_group_ids  = [aws_security_group.monitor.id]
+  key_name                = var.key_name
+  associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+                sudo apt-get install ca-certificates curl -y 
+                sudo install -m 0755 -d /etc/apt/keyrings -y
+                sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+                sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+                echo \
+                "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+                $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+                sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+                sudo apt-get update -y
+                sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+              EOF
+}
---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "region"{
+    default = "ap-southeast-2"
+}
+variable "instance_ami"{
+    default = "ami-080660c9757080771"
+}
+variable "instance_type"{
+    default = "t2.medium"
+}
+variable "key_name"{
+    default = "Ava"
+}
+variable "vpc_id"{
+    default = "vpc-0eadedaed1880f1ba"
+}
